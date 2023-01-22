ba51191aa2df5a593343ae1e50a4885c5696b078
abina16 <113548216+abina16@users.noreply.github.com>
2023-01-22 18:59:18 +0530

    Create q1.java

---
+++ q1.java
@@ -0,0 +1,31 @@
+/**Demonstrate how a parameter is passed to a servlet in a request object from HTML page.**/
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Trying extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    public Trying() {
+        super();
+    }
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		response.setContentType("text/html");
+		try {
+			String name = request.getParameter("name");
+			String pass = request.getParameter("pass");
+			out.print("Hi "+name+" ur pass is "+pass);
+		}
+		finally {
+			out.close();
+		}
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
